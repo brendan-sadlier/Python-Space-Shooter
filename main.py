40136cad87ab30cebcd9dd7df97ab80be628213d
Brendan Sadlier <brendan.sadlier@ucdconnect.ie>
2022-06-03 22:17:16 +0100

    Changing Spaceship Locations

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ WHITE = (255, 255, 255)  # RGB Code for White
 FPS = 60  # Game FPS
 SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT = 55, 40  # Dimensions for Spaceship Sprites
 
-BLUE_SHIP_IMG = pygame.image.load(os.path.join('Assets', 'shipBlue.png'))  # IMPORT BLUE SPACESHIP IMAGE
-GREEN_SHIP_IMG = pygame.image.load(os.path.join('Assets', 'shipGreen.png'))  # IMPORT GREEN SPACESHIP IMAGE
+GREEN_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipGreen.png')), 270)  # IMPORT GREEN SPACESHIP IMAGE
+BLUE_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipBlue.png')), 90)  # IMPORT BLUE SPACESHIP IMAGE
 
-BLUE_SHIP = pygame.transform.scale(BLUE_SHIP_IMG, (SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT))  # RESCALE BLUE SPACESHIP IMAGE
 GREEN_SHIP = pygame.transform.scale(GREEN_SHIP_IMG, (SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT))  # RESCALE GREEN SPACESHIP IMAGE
+BLUE_SHIP = pygame.transform.scale(BLUE_SHIP_IMG, (SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT))  # RESCALE BLUE SPACESHIP IMAGE
 
 
 def draw_window():
     WINDOW.fill(WHITE)  # Fill Screen With White Color
-    WINDOW.blit(BLUE_SHIP, (250, 675))  # Used to show Green Spaceship on screen
-    WINDOW.blit(GREEN_SHIP, (0, 225))  # Used to show Blue Spaceship on screen
+    WINDOW.blit(GREEN_SHIP, (300, 100))  # Used to show Green Spaceship on screen
+    WINDOW.blit(BLUE_SHIP, (700, 100))  # Used to show Blue Spaceship on screen
     pygame.display.update()  # Update Screen
 
 
