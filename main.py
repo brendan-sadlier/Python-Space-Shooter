b70f145b3e609350b8bf19f4e5e4ed93681c347d
Brendan Sadlier <brendan.sadlier@ucdconnect.ie>
2022-06-03 22:51:42 +0100

    Implement Basic Player Movement

--- main.py
+++ main.py
@@ -8,24 +8,54 @@ pygame.display.set_caption("Space Shooter")  # SET WINDOW TITLE
 
 WHITE = (255, 255, 255)  # RGB Code for White
 FPS = 60  # Game FPS
-SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT = 55, 40  # Dimensions for Spaceship Sprites
+VELOCITY = 5
+SHIP_WIDTH, SHIP_HEIGHT = 55, 40  # Dimensions for Spaceship Sprites
 
-GREEN_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipGreen.png')), 270)  # IMPORT GREEN SPACESHIP IMAGE
-BLUE_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipBlue.png')), 90)  # IMPORT BLUE SPACESHIP IMAGE
+GREEN_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipGreen.png')),
+                                         270)  # IMPORT GREEN SPACESHIP IMAGE
+BLUE_SHIP_IMG = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'shipBlue.png')),
+                                        90)  # IMPORT BLUE SPACESHIP IMAGE
 
-GREEN_SHIP = pygame.transform.scale(GREEN_SHIP_IMG, (SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT))  # RESCALE GREEN SPACESHIP IMAGE
-BLUE_SHIP = pygame.transform.scale(BLUE_SHIP_IMG, (SHIP_IMG_WIDTH, SHIP_IMG_HEIGHT))  # RESCALE BLUE SPACESHIP IMAGE
+GREEN_SHIP = pygame.transform.scale(GREEN_SHIP_IMG, (SHIP_WIDTH, SHIP_HEIGHT))  # RESCALE GREEN SPACESHIP IMAGE
+BLUE_SHIP = pygame.transform.scale(BLUE_SHIP_IMG, (SHIP_WIDTH, SHIP_HEIGHT))  # RESCALE BLUE SPACESHIP IMAGE
 
 
-def draw_window():
+def draw_window(green, blue):
     WINDOW.fill(WHITE)  # Fill Screen With White Color
-    WINDOW.blit(GREEN_SHIP, (300, 100))  # Used to show Green Spaceship on screen
-    WINDOW.blit(BLUE_SHIP, (700, 100))  # Used to show Blue Spaceship on screen
+    WINDOW.blit(GREEN_SHIP, (green.x, green.y))  # Used to show Green Spaceship on screen
+    WINDOW.blit(BLUE_SHIP, (blue.x, blue.y))  # Used to show Blue Spaceship on screen
     pygame.display.update()  # Update Screen
+
+
+# HANDLE GREEN SPACESHIP MOVEMENT FROM PLAYER INPUT
+def green_movement_handler(keys_pressed, green):
+    if keys_pressed[pygame.K_a]:  # LEFT
+        green.x -= VELOCITY
+    if keys_pressed[pygame.K_d]:  # RIGHT
+        green.x += VELOCITY
+    if keys_pressed[pygame.K_w]:  # UP
+        green.y -= VELOCITY
+    if keys_pressed[pygame.K_s]:  # DOWN
+        green.y += VELOCITY
+
+
+# HANDLE GREEN SPACESHIP MOVEMENT FROM PLAYER INPUT
+def blue_movement_handler(keys_pressed, blue):
+    if keys_pressed[pygame.K_LEFT]:  # LEFT
+        blue.x -= VELOCITY
+    if keys_pressed[pygame.K_RIGHT]:  # RIGHT
+        blue.x += VELOCITY
+    if keys_pressed[pygame.K_UP]:  # UP
+        blue.y -= VELOCITY
+    if keys_pressed[pygame.K_DOWN]:  # DOWN
+        blue.y += VELOCITY
 
 
 # Main Function
 def main():
+    green = pygame.Rect(100, 300, SHIP_WIDTH, SHIP_HEIGHT)
+    blue = pygame.Rect(700, 300, SHIP_WIDTH, SHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -38,7 +68,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        green_movement_handler(keys_pressed, green)
+        blue_movement_handler(keys_pressed, blue)
+
+        draw_window(green, blue)
 
     pygame.quit()
 
